2c8fd2314fe1eaa6266b900cafa6f099b47f82e2
nguyenquangmai <nguyenquangmai249@gmail.com>
2025-11-23 00:38:59 +0700

    added next solution :remove element

---
+++ LeetCodeSolutions/Easy/RemoveElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    internal class RemoveElement
+    {
+        public static void DisplaySolution()
+        {
+            int[] nums1 = new int[] { 3,2,2,3 };
+            int[] nums2 = new int[] { 0,1,2,2,3,0,4,2 };
+
+            Console.WriteLine(Solution(nums1, 3));
+            Console.WriteLine();
+            Console.WriteLine(Solution(nums2, 2));
+        }
+
+        private static int Solution(int[] nums, int val)
+        {
+            int res = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[res] = nums[i];
+                    res++;
+                }
+            }
+            
+            return res;
+        }
+    }
+}
