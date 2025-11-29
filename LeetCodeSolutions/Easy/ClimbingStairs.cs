ef50681e349103015a9992322596b257a8172a35
nguyenquangmai <nguyenquangmai249@gmail.com>
2025-11-30 02:28:00 +0700

    added next easy solution : Climb Stairs

---
+++ LeetCodeSolutions/Easy/ClimbingStairs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    internal class ClimbingStairs
+    {
+        public static void DisplaySolution()
+        {
+            Console.WriteLine(Solution(2));
+            Console.WriteLine(Solution(3));
+            Console.WriteLine(Solution(4));
+            Console.WriteLine(Solution(5));
+            Console.WriteLine(Solution(6));
+        }
+
+        private static int Solution(int n)
+        {
+            if (n <= 3)
+                return n;
+
+            int f1 = 3;
+            int f2 = 2;
+            int res = 0;
+
+            for(int i = 4; i <= n; i++)
+            {
+                res = f1 + f2;
+                f2 = f1;
+                f1 = res;
+            }
+
+            return res;
+        }
+    }
+}
