b51a6c6c73eb92db2a02c36810df1afd332e5f24
nguyenquangmai <nguyenquangmai249@gmail.com>
2025-12-02 04:55:44 +0700

    added next easy solution : get max depth of binary tree

---
+++ LeetCodeSolutions/Easy/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Easy
+{
+    internal class MaximumDepthOfBinaryTree
+    {
+        public static void DisplaySolution()
+        {
+            TreeNode root1 = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            TreeNode root2 = new TreeNode(1, null, new TreeNode(2));
+
+            Console.WriteLine(MaxDepth(root1));
+            Console.WriteLine(MaxDepth(root2));
+        }
+
+        private static int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        }
+    }
+}
